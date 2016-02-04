5621e59591c9f6f0328e2874b5e947c8d32e17b6
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2016-02-04 18:05:54 -0200

    O código foi alterado para que quando o som termine a tecla pressionada se solte automaticamente

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 var tones = [];
 
-function init(){
-    var w = document.getElementsByClassName("key");
+function init(){ 
+   var w = document.getElementsByClassName("key");
 
     for(var i = 0; i < w.length; i++){
+        tones[i] = {};
 
-        tones[i] = new Audio();   
-        tones[i].src = "/ogg/"+w[i].id+".ogg";
+        tones[i].sound = new Audio();   
+        tones[i].sound.src = "/ogg/"+w[i].id+".ogg";
+
+        tones[i].key = w[i];
 
         w[i].value = i;
         w[i].played = false;
@@ -20,27 +23,44 @@ function init(){
 }
 
 function addEvent(w, class_name){
+    w.addEventListener("mousedown", function(e){
+        $(this).addClass(class_name);
+        playTone(this.value);        
+    }, false)
+
+    w.addEventListener("mouseup", function(e){
+        $(this).removeClass(class_name);
+        stopTone(this.value);        
+    }, false)
+
     w.addEventListener("touchstart",function(e){
         $(this).addClass(class_name);            
         playTone(this.value);
-        this.played = true;
-        console.log(e);
     }, true);
+
 
     w.addEventListener("touchend",function(){
         $(this).removeClass(class_name);   
         stopTone(this.value);
-        this.played = false;         
     }, true);                
 }
 
 function playTone(key) {
-    tones[key].play();    
+    tones[key].sound.play();    
+    _target = tones[key];
+    tones[key].sound.onended = function(){
+        if(_target.key.id.indexOf("b") == -1){
+            $(_target.key).removeClass("whitepressed");
+        }
+        else{
+            $(_target.key).removeClass("blackpressed");
+        }
+    }
 };
 
 function stopTone(key){
-    tones[key].pause();
-    tones[key].currentTime=0;
+    tones[key].sound.pause();
+    tones[key].sound.currentTime=0;
 }
 
 $(document).ready(function(){
